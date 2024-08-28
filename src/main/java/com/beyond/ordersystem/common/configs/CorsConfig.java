d8ab061d659daa2242a4f6364ef11d70fec8e27e
tteia <tteia.dl@gmail.com>
2024-08-28 11:44:15 +0900

    배포 테스트

--- src/main/java/com/beyond/ordersystem/common/configs/CorsConfig.java
+++ src/main/java/com/beyond/ordersystem/common/configs/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://www.tteia.xyz") // 허용 url 명시 (우리 서버에 들어올 수 있는 url 명시)
+                .allowedOrigins("https://www.tteia.xyz") // 허용 url 명시 (우리 서버에 들어올 수 있는 url 명시)
                 .allowedMethods("*") // GET, POST, PUT, DELETE 등 요청
                 .allowedHeaders("*")
                 .allowCredentials(true);
